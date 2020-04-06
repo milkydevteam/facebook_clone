148e0a68f66b21d7b82c083f2ea658240273f96c
chienvm2 <chienvm2@fpt.com.vn>
2020-04-06 21:05:44 +0700

    update login screen

--- src/screens/auths/LoginScreen.js
+++ src/screens/auths/LoginScreen.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './src/screens/auths/LoginScreen';
 
-export default function App() {
+export default function LoginScreen() {
   return (
     <View style={styles.container}>
-      <LoginScreen />
+      <Text>Login Page!</Text>
     </View>
   );
 }
